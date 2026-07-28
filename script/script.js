5cd96de18b5c2d4561df4ab9853e05aae9790c16
Monisha-star18 <monisha.r180505@gmail.com>
2026-07-28 12:55:19 +0530

    displaying numbers in calculator window and basic function

--- script/script.js
+++ script/script.js
@@ -15,8 +15,35 @@ $(".num-pallet-sy-btn").click(function()
     setTimeout(function(){clickedButton.removeClass("selectedsy")},1000)
 })
 
-
+// display the clicked number on calculator window 
 $(".num-pallet-btn").click(function(){
-    let clickedButtonValue = $(this).text()
-    
+    if (this.id != 'backspace')
+    {
+        let clickedButtonValue = $(this).text()
+        $(".typingSpace").append(clickedButtonValue)
+    }
 })
+
+$(".num-pallet-sy-btn").click(function(){
+    if (this.id != 'clear' && this.id != 'bracket' && this.id != 'eqaulTo')
+    {
+        let clickedButtonValue = $(this).text()
+        $(".typingSpace").append(clickedButtonValue)
+    }
+})
+
+// AC clear all
+$("#clear").click(function()
+{
+    $(".typingSpace").text("")
+})
+
+// //backspace
+$("#backspace").click(function() {
+    // get value from typing
+    let currentValue = $(".typingSpace").text().trim();
+    // remove the last elemnt
+    let editedValue = currentValue.slice(0, -1);
+    //push again
+    $(".typingSpace").text(editedValue);
+});
